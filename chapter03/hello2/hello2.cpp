65d2a451299b035a2ef567be776e4b442363bc38
chmod644 <chmod644@github.com>
2014-01-14 21:42:53 +0900

    finish chapter02-03

---
+++ chapter03/hello2/hello2.cpp
@@ -0,0 +1,12 @@
+/**************************************************
+ * hello --- print "Hello World" on concole
+ * author: Steve Oulline
+ **************************************************/
+
+#include <iostream>
+int main(void)
+{
+	// print "Hello World"
+	std::cout << "Hello World\n";
+	return 0;
+}
